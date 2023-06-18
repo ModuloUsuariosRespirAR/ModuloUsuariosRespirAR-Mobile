e9d8bfd92f696f43fe3ca691611a9b5a3dc289df
DiegoT2 <diego.tomassetti97@gmail.com>
2023-06-18 16:08:00 -0300

    Creacion de usuario basica

--- src/screens/UserList.js
+++ src/screens/UserList.js
@@ -1,17 +1,89 @@
 import { useAuth } from '../context/authContext';
-import { ScrollView } from 'native-base';
+import { ScrollView, useToast } from 'native-base';
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { View, Text, FlatList, StyleSheet, Modal, Animated, TextInput, Pressable } from 'react-native';
+import { Button, IconButton } from '@react-native-material/core';
+import Icon from '@expo/vector-icons';
 import { DataTable, Switch } from 'react-native-paper';
+
+
+import { SelectList } from 'react-native-dropdown-select-list';
 
 const UserList = () => {
   const [isSwitchOn, setIsSwitchOn] = React.useState(true);
-  const { loadUsers, usersList } = useAuth();
+  const { loadUsers, usersList, token, acessToken } = useAuth();
   const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
   
+  const [fadeAnim] = useState(new Animated.Value(0));
+  const [showAddUsrModal, setShowAddUsrModal] = useState(false);
+
+  const {createNewUser, createdUser} = useAuth();
+
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [selectedRole, setSelectedRole] = React.useState('');
+
+  const toast = useToast();
+
 
   useEffect(() => loadUsers(), []);
  
+  const handleAddUser = () => {
+    setShowAddUsrModal(true);
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true
+    }).start();
+  };
+
+  const handleCloseAddUsrModal = () => {
+    Animated.timing(fadeAnim, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true
+    }).start(() => {
+      setShowAddUsrModal(false);
+    });
+  };
+
+
+  const handleCreateUser = async () => {
+    if (username === '' || email === '') {
+      return toast.show({
+        description: 'Todos los campos son obligatorios'
+      });
+    }
+
+    console.log("token", token)
+
+    try {
+      const result = await createNewUser(
+        token,
+        acessToken,
+        username,
+        username,
+        email,
+        "1234"
+      );
+      if (result === null) {
+        toast.show({
+          description:
+            'El usuario ó contraseña ingresado es incorrecto ó no se encuentra registrado'
+        });
+      }
+    } catch (error) {
+      toast.show({
+        description: 'Hubo un error, intente nuevamente'
+      })
+      console.log("error", error)
+      ;
+    }
+
+
+    useEffect(() => loadUsers(), []);
+  };
+
   const styles = StyleSheet.create({
     container: {
       paddingTop: 20,
@@ -22,17 +94,72 @@ const UserList = () => {
       backgroundColor: '#DCDCDC'
     },
     title: {
-      width: 160
+      width: 85
     },
     cell: {
-      width: 100
+      width: 85
     },
     switch: {
       width: 50
+    },
+    mainContainer: {
+      flex: 1
+    },
+    container: {
+      flex: 1,
+      alignItems: 'center'
+      /* justifyContent: 'center' */
+    },
+    image: {
+      flex: 1,
+      justifyContent: 'center'
+    },
+    mainText: {
+      color: '#00CEFE',
+      fontSize: 40,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      marginTop: 50
+    },
+    userNameText: {
+      margin: 10,
+      fontSize: 18,
+      fontWeight: 'bold'
+    },
+    textContainer: {
+      backgroundColor: '#F29D35',
+      borderRadius: 10,
+      padding: 10,
+      marginBottom: 5,
+      marginHorizontal: 10,
+      elevation: 5
+    },
+    notificationsContainer: {
+      marginBottom: 25
+    },
+    modalContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    modalContent: {
+      backgroundColor: '#fff',
+      borderRadius: 10,
+      padding: 20,
+      opacity: fadeAnim
+    },
+    modalInput: {
+      height: 40,
+      width: 200,
+      marginVertical: 10,
+      borderWidth: 1,
+      padding: 10
     }
+ 
   });
 
-  const headers = ['Username', 'Email', 'Habilitado', 'Fecha Password'];
+  const headers = ['Username', 'Email', 'Habilitado', ''];
+  //const headers = ['Username', 'Email', 'Habilitado', 'Fecha Password'];
 
   return (
     <View style={styles.container}>
@@ -42,6 +169,11 @@ const UserList = () => {
         keyExtractor={(item) => item.id.toString()}
         renderItem={({ item }) => <Text>{item.name}</Text>}
       /> */}
+      <Button
+              title="Agregar Usuario"
+              color="#00CEFE"
+              onPress={handleAddUser}
+            />
       <ScrollView horizontal>
         <DataTable>
           <DataTable.Header style={styles.tableHeader}>
@@ -68,13 +200,67 @@ const UserList = () => {
                   />
                 </DataTable.Cell>
                 <DataTable.Cell style={styles.cell}>
-                  <Text>{user.date_password}</Text>
+                <Text>Edit</Text>
+
+                {/* 
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                  
+                  <Pressable onPress={() => console.log(user.username + ", edit: " + user.email)}>
+                    <Icon name="pencil"/>
+                  </Pressable>
+                  
+                  <Pressable onPress={() => console.log(user.username + ", delete: " + user.email)}>
+                    <Icon name="delete-forever"/>
+                  </Pressable>
+
+                </View> 
+                */}
+                  
                 </DataTable.Cell>
               </DataTable.Row>
             ))
           )}
         </DataTable>
       </ScrollView>
+    
+
+      <Modal
+          visible={showAddUsrModal}
+          transparent={true}
+          onRequestClose={handleCloseAddUsrModal}
+        >
+          <View style={styles.modalContainer}>
+            <Animated.View style={[styles.modalContent]}>
+              <Text style={styles.loginText}>Agregar Usuario</Text>
+              <TextInput
+                  style={styles.modalInput}
+                  value={username}
+                  placeholder="Username"
+                  onChangeText={(text) => setUsername(text)}
+                />
+              {/* <TextInput style={styles.modalInput} placeholder="Email" /> */}
+              <TextInput
+                  style={styles.modalInput}
+                  value={email}
+                  placeholder="Email"
+                  onChangeText={(text) => setEmail(text)}
+                />
+                            
+              <Button
+                color="#359AF2"
+                title="Agregar"
+                onPress={handleCreateUser}
+              />
+              <Button
+                color="#FF0000"
+                title="Cancelar"
+                onPress={handleCloseAddUsrModal}
+              />
+            </Animated.View>           
+          </View>
+        </Modal>
+    
+    
     </View>
   );
 };
--- src/services/user.js
+++ src/services/user.js
@@ -7,6 +7,7 @@ export const login = async ({ data }) => {
       Constants.manifest.extra.backendUrl + '/login',
       data
     );
+    console.log(response.data)
     return response.data;
   } catch (error) {
     throw new error();
@@ -39,5 +40,64 @@ export const getRoles = async ({ token }) => {
     return response.data;
   } catch (error) {
     throw new error();
+  }
+  
+};
+
+
+export const createUser = async (
+  token,
+  acessToken,
+  displayName,
+  username,
+  email,
+  password
+) => {
+  const user = {
+    displayName: displayName,
+    username: username,
+    email: email,
+    password: password,
+  };
+
+  console.log("user data", user)
+
+  console.log("Login authToken; accessToken", token + "; " + acessToken)
+  
+  const result = await axios
+    .post(
+      Constants.manifest.extra.backendUrl  + "/users/create",
+      {
+        user,
+      },
+      {
+        headers: { "X-Auth-token": token, accesstoken: acessToken },
+      }
+    )
+    console.log("result", result)
+    .then((res) => {
+      return res.data;
+    })
+    .catch((error) => {
+      if (error.response) {
+        return {
+          error: {
+            statusCode: error.response.status,
+            message: error.response.data,
+          },
+        };
+      } else {
+        return {
+          error: {
+            statusCode: 500,
+            message: "Keyrock connection failed",
+          },
+        };
+      }
+    });
+  if (result.access_token) {
+    return result.access_token;
+  } else {
+    return result;
   }
 };
